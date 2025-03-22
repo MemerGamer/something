adbac679ddb78e4d04ed0bd75605512e720f6344
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2025-03-22 09:41:16 +0200

    fix(app): fixed text color for h1

--- app/src/components/atoms/H1.tsx
+++ app/src/components/atoms/H1.tsx
@@ -9,9 +9,7 @@ type H1Props = {
 
 const H1 = ({ children, accent }: H1Props) => {
   const styles = useThemedStyles();
-  return (
-    <Text style={[styles.H1, { color: styles.alwaysWhiteText.color }, accent && styles.headerAccent]}>{children}</Text>
-  );
+  return <Text style={[styles.H1, { color: styles.text.color }, accent && styles.headerAccent]}>{children}</Text>;
 };
 
 export default H1;
