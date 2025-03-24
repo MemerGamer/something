f23b732715cab5eb30a673152874a99aa51220ee
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2025-03-24 20:48:22 +0200

    fix: working on backend file upload error

--- api/src/utils/errors.ts
+++ api/src/utils/errors.ts
@@ -50,6 +50,6 @@ export const globalErrorHandler: ErrorHandler = (error, c) => {
   if (error instanceof HTTPException && error.res?.status === StatusCodes.UNAUTHORIZED) {
     return c.json(reasonPhrase(StatusCodes.UNAUTHORIZED), StatusCodes.UNAUTHORIZED);
   }
-  console.error(c.body);
+  console.error(c.req.parseBody());
   return c.json(reasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR), StatusCodes.INTERNAL_SERVER_ERROR);
 };
